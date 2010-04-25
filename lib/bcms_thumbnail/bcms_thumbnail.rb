dbee4e0c98ed29dae763fc0379e3871d1311073d
djcp <dan@collispuro.com>
2010-04-25 11:53:51 -0400

    Fix file permissions

--- lib/bcms_thumbnail/bcms_thumbnail.rb
+++ lib/bcms_thumbnail/bcms_thumbnail.rb
@@ -10,10 +10,13 @@ module ActionView
         if ! File.exists?("#{RAILS_ROOT}/public#{thumbnail_location}")
           if ! File.exists?("#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{geometry}")
             FileUtils.mkdir_p("#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{geometry}")
+            FileUtils.chmod 0755, "#{RAILS_ROOT}/public/bcms_thumbnail_cache/"
+            FileUtils.chmod 0755, "#{RAILS_ROOT}/public/bcms_thumbnail_cache/#{geometry}"
           end
           image = MiniMagick::Image.from_file("#{RAILS_ROOT}/tmp/uploads/#{attachment_obj.attachment.file_location}")
           image.resize geometry
           image.write("#{RAILS_ROOT}/public#{thumbnail_location}")
+          FileUtils.chmod 0644, "#{RAILS_ROOT}/public#{thumbnail_location}"
           URI::escape(thumbnail_location)
         else
           URI::escape(thumbnail_location)
